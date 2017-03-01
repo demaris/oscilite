68147e8c4cb46a341f1b30c8d129460174225487
demaris <davidl.demaris@gmail.com>
2017-03-01 00:31:14 -0800

    refactor to remove globals

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 from setuptools import setup
 
-setup(name='cml_basic'
-    version='0.1',
+setup(name='cml_basic',
+    version='0.1.2',
     description='basic diffusive coupled map lattice and analysis utilities',
     url='http://github.com/demaris/cml_basic',
     author='demaris',
     author_email='davidl.demaris@gmail.com',
     license='MIT',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 2.7',
+    ],	
     packages=['cml_basic'],
     data_files=[("",["LICENSE.txt"])],
-    install_requires=[
-        numpy,
-        scipy.stats,
-        scipy.signal,
-        matplotlib,
-    ],
     zip_safe=False)
